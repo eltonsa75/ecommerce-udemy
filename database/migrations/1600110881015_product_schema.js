714e813a33cd31a0b34028dc9264e53a5a7a3e79
Elton <eltonsa75@hotmail.com>
2020-09-14 20:34:31 -0300

    Order Schema

--- database/migrations/1600110881015_product_schema.js
+++ database/migrations/1600110881015_product_schema.js
@@ -31,15 +31,28 @@ class ProductSchema extends Schema {
       .inTable('images')
       .onDelete('cascade')
 
-      table.foreign('product_id')
+      table
+      .foreign('product_id')
       .references('id')
       .inTables('products')
       .onDelete('cascade')
 
     })
+    this.create('category_product', table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table
+      .foreign('prduct_id')
+      .references('id')
+      .inTable('products')
+      .onDelete('cascade')
+    })
   }
 
   down () {
+    this.drop('category_product')
     this.drop('image_products')
     this.drop('products')
   }
